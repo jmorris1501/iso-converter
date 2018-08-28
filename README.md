dd9ef2df35a02ca94f1235f09dff5d4c16c2cb32
jmorris1501 <jmorris1501@gmail.com>
2018-08-28 21:54:52 +0200

    schema generation
    found schemas, started on creation

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 ## Description
 
 An Iso8583 and Iso20022 converter
+
+## Building
+
+To build library, run the following command:
+
+*mvn clean package*
+
+and check the *target/* directory for the generated jar
--- src/main/java/com/iso/converter/IsoConverter.java
+++ src/main/java/com/iso/converter/IsoConverter.java
@@ -1,5 +1,7 @@
 package com.iso.converter;
 
+import iso.std.iso._20022.tech.xsd.pain_001_001.CustomerCreditTransferInitiationV08;
+import iso.std.iso._20022.tech.xsd.pain_002_001.CustomerPaymentStatusReportV09;
 import org.bitbucket.openisoj.Iso8583;
 import org.bitbucket.openisoj.Iso8583Rev93;
 
@@ -9,35 +11,41 @@ import javax.xml.bind.Unmarshaller;
 import java.io.StringReader;
 
 @SuppressWarnings("unused")
-public class IsoConverter
-{
-	 /**
-	  * create an Iso8583 message from a byte array
-	  *
-	  * @param bytes the byte array
-	  * @return the Iso8583 message
-	  * @throws Exception in case of error unpacking the array
-	  */
-	 public static Iso8583 getIso8583(byte[] bytes) throws Exception
-	 {
-		  Iso8583 msg = new Iso8583();
-		  msg.unpack(bytes, 0);
-		  return msg;
-	 }
+public class IsoConverter {
+	/**
+	 * create an Iso8583 message from a byte array
+	 *
+	 * @param bytes the byte array
+	 * @return the Iso8583 message
+	 * @throws Exception in case of error unpacking the array
+	 */
+	public static Iso8583 getIso8583(byte[] bytes) throws Exception {
+		Iso8583 msg = new Iso8583();
+		msg.unpack(bytes, 0);
+		return msg;
+	}
 
-	 /**
-	  * create an Iso8583Rev93 message from a byte array
-	  *
-	  * @param bytes the byte array
-	  * @return the Iso8583Rev93 message
-	  * @throws Exception in case of error unpacking the array
-	  */
-	 public static Iso8583Rev93 getIso8583Rev93(byte[] bytes) throws Exception
-	 {
-		  Iso8583Rev93 msg = new Iso8583Rev93();
-		  msg.unpack(bytes, 0);
-		  return msg;
-	 }
+	/**
+	 * create a customer credit transfer initiation message from the string xml data
+	 *
+	 * @param data string xml data
+	 * @return customer credit transfer initiation object
+	 * @throws JAXBException in case of error parsing xml
+	 */
+	public static CustomerCreditTransferInitiationV08 getCustomerCreditTransferInitiation(String data) throws JAXBException {
+		return ((iso.std.iso._20022.tech.xsd.pain_001_001.Document) unMarshal(data)).getCstmrCdtTrfInitn();
+	}
+
+	/**
+	 * create a customer payment status report message from the string xml data
+	 *
+	 * @param data string xml data
+	 * @return customer payment status report object
+	 * @throws JAXBException in case of error parsing xml
+	 */
+	public static CustomerPaymentStatusReportV09 getCustomerPaymentStatusReport(String data) throws JAXBException {
+		return ((iso.std.iso._20022.tech.xsd.pain_002_001.Document) unMarshal(data)).getCstmrPmtStsRpt();
+	}
 
 	/**
 	 * return an object representing an Iso20022 message
@@ -46,7 +54,7 @@ public class IsoConverter
 	 * @return the message object
 	 * @throws JAXBException in case of error while parsing the xml string
 	 */
-	public static Object unMarshal(String data) throws JAXBException {
+	private static Object unMarshal(String data) throws JAXBException {
 		//TODO object types, see if possible to pass in type or if reading return is safe
 		JAXBContext jaxbContext = JAXBContext.newInstance(Object.class);
 		Unmarshaller jaxbUnMarshaller = jaxbContext.createUnmarshaller();

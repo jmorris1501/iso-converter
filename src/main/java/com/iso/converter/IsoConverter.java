737b6331ba06a053609a42ca02b884f9167df625
Justin Morris <justin.morris@stanchionpayments.com>
2018-08-29 18:17:47 +0200

    framework methods
    framework methods for conversion, field mapping still required

--- src/main/java/com/iso/converter/IsoConverter.java
+++ src/main/java/com/iso/converter/IsoConverter.java
@@ -3,14 +3,13 @@ package com.iso.converter;
 import iso.std.iso._20022.tech.xsd.pain_001_001.CustomerCreditTransferInitiationV08;
 import iso.std.iso._20022.tech.xsd.pain_002_001.CustomerPaymentStatusReportV09;
 import org.bitbucket.openisoj.Iso8583;
-import org.bitbucket.openisoj.Iso8583Rev93;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.io.StringReader;
 
-@SuppressWarnings("unused")
+@SuppressWarnings({"unused", "UnnecessaryLocalVariable"})
 public class IsoConverter {
 	/**
 	 * create an Iso8583 message from a byte array
@@ -46,6 +45,54 @@ public class IsoConverter {
 	public static CustomerPaymentStatusReportV09 getCustomerPaymentStatusReport(String data) throws JAXBException {
 		return ((iso.std.iso._20022.tech.xsd.pain_002_001.Document) unMarshal(data)).getCstmrPmtStsRpt();
 	}
+
+	 /**
+	  * convert an Iso8583 0100 request message to a CustomerCreditTransferInitiationV08 message
+	  *
+	  * @param iso8583 iso8583 0100 request message
+	  * @return CustomerCreditTransferInitiationV08 message
+	  */
+	 public static CustomerCreditTransferInitiationV08 convert85830100to20022(Iso8583 iso8583) {
+		  CustomerCreditTransferInitiationV08 iso20022 = new CustomerCreditTransferInitiationV08();
+		  //TODO map fields
+		  return iso20022;
+	 }
+
+	 /**
+	  * convert an Iso8583 0110 response message to a CustomerPaymentStatusReportV09 message
+	  *
+	  * @param iso8583 iso 8583 0110 response message
+	  * @return CustomerPaymentStatusReportV09 message
+	  */
+	 public static CustomerPaymentStatusReportV09 convert85830110to20022(Iso8583 iso8583) {
+		  CustomerPaymentStatusReportV09 iso20022 = new CustomerPaymentStatusReportV09();
+		  //TODO map fields
+		  return iso20022;
+	 }
+
+	 /**
+	  * convert a CustomerCreditTransferInitiationV08 message to an Iso8583 0100 request message
+	  *
+	  * @param iso20022 CustomerCreditTransferInitiationV08 message
+	  * @return iso8583 0100 request message
+	  */
+	public static Iso8583 convert20022CCTIto85830100(CustomerCreditTransferInitiationV08 iso20022) {
+		 Iso8583 iso8583 = new Iso8583();
+		 //TODO map fields
+		 return iso8583;
+	}
+
+	 /**
+	  * convert a CustomerPaymentStatusReportV09 message to an Iso8583 0110 response message
+	  *
+	  * @param iso20022 CustomerPaymentStatusReportV09 message
+	  * @return iso 8583 0110 response message
+	  */
+	 public static Iso8583 convert20022CPSRto85830100(CustomerCreditTransferInitiationV08 iso20022) {
+		  Iso8583 iso8583 = new Iso8583();
+		  //TODO map fields
+		  return iso8583;
+	 }
 
 	/**
 	 * return an object representing an Iso20022 message
